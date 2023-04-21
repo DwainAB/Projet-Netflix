6e62412c88dca432c13029c5160c347061141fa6
Aghiles Haddar <haddaraghiles5@gmail.com>
2023-04-21 14:41:30 +0200

    ajout du nouveau lien

--- Projet-Netflix/src/Components/CallApi/CallApi.jsx
+++ Projet-Netflix/src/Components/CallApi/CallApi.jsx
@@ -1,19 +1,7 @@
 import React, {useEffect, useState} from 'react';
 
 function CallApi(){
-    const url = "https://api.themoviedb.org/3/movie/550?api_key=3c55b27b9e2cf84477c1a7e17da7a0bf"
-    const [userData, setUserData] = useState('')
-    const getData = async (url) =>{
-        const rowData = await fetch(url)
-        const jsonData = await rowData.json()
-        setUserData(jsonData)
-      }
 
-      useEffect(() =>{
-        getData(url)
-      }, [])
-
-      console.log(userData.title);
 }
 
 export default CallApi
